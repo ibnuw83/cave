872ddeaa86dfc7c80fe2a818c549e0d55daf8b4a
Ibnu Wibowo <ibnuw83@gmail.com>
2025-12-18 08:45:12 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://www.keb

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'www.kebumenkab.go.id',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   env: {
