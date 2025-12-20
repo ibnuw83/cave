7ecee3727844394d4df525a321bea7fe0cb907e0
Ibnu Wibowo <ibnuw83@gmail.com>
2025-12-20 05:19:32 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.js
+++ next.config.js
@@ -86,6 +86,12 @@ const nextConfig = {
         hostname: 'blogger.googleusercontent.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'salsawisata.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
