a91838e604dcc3bcc8e5e41bde9d6f3ed10fa5c1
Ibnu Wibowo <ibnuw83@gmail.com>
2025-12-21 13:01:18 +0000

    header halaman apa , tidak muncul

--- src/app/components/spot-player-ui.tsx
+++ src/app/components/spot-player-ui.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useState, useEffect, useRef, useCallback } from 'react';
@@ -513,5 +514,7 @@ export default function SpotPlayerUI({ spot, userRole, allSpots, vrMode = false,
     </>
   );
 }
+
+    
 
     
