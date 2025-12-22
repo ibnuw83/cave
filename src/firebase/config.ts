3acf797ad7600393344006a92b14b912ef81c06d
Ibnu Wibowo <ibnuw83@gmail.com>
2025-12-22 15:34:34 +0000

    const firebaseConfig = {   apiKey: "AIzaSyAr40uWMHG9Wdqa5D13K_jAKc_gIy4J

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyAr40uWMHG9Wdqa5D13K_jAKc_gIy4Jrg8",
   authDomain: "cave-57567.firebaseapp.com",
   projectId: "cave-57567",
-  storageBucket: "cave-57567.appspot.com",
+  storageBucket: "cave-57567.firebasestorage.app",
   messagingSenderId: "862428789556",
   appId: "1:862428789556:web:0941e0856737894942c361",
   measurementId: "G-7DTNV0C4ZV"
