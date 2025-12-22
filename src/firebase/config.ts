9b63c9e3b6cb61db83ae933e01061190aa6417cd
Ibnu Wibowo <ibnuw83@gmail.com>
2025-12-22 14:02:04 +0000

    const firebaseConfig = {   apiKey: "AIzaSyAr40uWMHG9Wdqa5D13K_jAKc_gIy4J

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAr40uWMHG9Wdqa5D13K_jAKc_gIy4Jrg8",
+  authDomain: "cave-57567.firebaseapp.com",
+  projectId: "cave-57567",
+  storageBucket: "cave-57567.appspot.com",
+  messagingSenderId: "862428789556",
+  appId: "1:862428789556:web:0941e0856737894942c361",
+  measurementId: "G-7DTNV0C4ZV"
 };
