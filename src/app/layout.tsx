fc0f072dc55060f2e7c4163261f653f81394b79b
Ibnu Wibowo <ibnuw83@gmail.com>
2025-12-29 06:24:19 +0000

    apa bisa di buat PWA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,8 +15,13 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Rye&display=swap" rel="stylesheet" />
+        
+        {/* PWA Tags */}
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#2A2B32" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="apple-mobile-web-app-status-bar" content="#2A2B32" />
+        
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         
